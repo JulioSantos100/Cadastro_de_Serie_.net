18a41ae5b8c2365d27ebd61e8838b5c18862c15f
JulioSantos100 <juliodsantos2016@gmail.com>
2021-08-21 18:35:03 -0300

    Cadastro de Serie)

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+	public class SerieRepositorio : IRepositorio<Serie>
+	{
+        private List<Serie> listaSerie = new List<Serie>();
+		public void Atualiza(int id, Serie objeto)
+		{
+			listaSerie[id] = objeto;
+		}
+
+        public void Atualizar(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+		{
+			listaSerie[id].Excluir();
+		}
+
+        public void Excluir(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie objeto)
+		{
+			listaSerie.Add(objeto);
+		}
+
+		public List<Serie> Lista()
+		{
+			return listaSerie;
+		}
+
+		public int ProximoId()
+		{
+			return listaSerie.Count;
+		}
+
+		public Serie RetornaPorId(int id)
+		{
+			return listaSerie[id];
+		}
+	}
+}
